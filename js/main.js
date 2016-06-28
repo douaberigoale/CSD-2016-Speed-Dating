8ca0dbae8913d3a3558db5fc9ca73ac1df256918
Iris <iris.banciu@gmail.com>
2016-06-28 22:25:30 +0200

    almost done.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
+$(function () {
+    $('#datetimepicker').datetimepicker({
+        viewMode: 'years',
+        format: 'YYYY'
+    });
+});
+/* not needed now, maybe later for data usage.
+ function test() {
+ var name = document.getElementById("name").value;
+ var gender = document.getElementById('input[name="sexuality"]:checked').required;
+ var orientation = document.getElementById('input[name="sexuality"]:checked').required;
+ document.getElementById("name").innerHTML = name;
+ document.getElementById("gender").innerHTML = gender;
+ document.getElementById("sexuality").innerHTML = orientation;
 
-
+ }*/
